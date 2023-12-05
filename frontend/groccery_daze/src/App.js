f878a679ebcf6eb9f5b527f11a2b156614875120
GogogoDavis <burkedavis@yahoo.com>
2023-12-04 18:14:23 -0700

    thing be happen?

--- frontend/groccery_daze/src/App.js
+++ frontend/groccery_daze/src/App.js
@@ -1,34 +1,62 @@
 // Parent component (e.g., App.js)
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AddItem } from './components/AddItem';
 
-const App = () => {
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-  const openPopup = () => {
-    setIsPopupOpen(true);
+
+const App = () => {
+
+  const [ItemArray, setItemArray] = useState([]);
+  const [AddWindow, setAddWindow] = useState(false);
+
+  const fetchData = () => {
+    fetch( `http://localhost:8080`)
+    .then(res => res.json())
+    .then(data => setItemArray(data));
+  }
+
+  useEffect((e) => {
+  fetchData(); 
+  }, [AddWindow]);
+
+  console.log(ItemArray); 
+  //returns all data, map through this and add the addWindow
+ 
+
+  const openWindow = () => {
+    setAddWindow(true);
   };
 
-  const closePopup = () => {
-    setIsPopupOpen(false);
+  const closeWindow = () => {
+    setAddWindow(false);
+    fetchData();
   };
 
   return (
+    <>
+    
     <div>
-      <h1>Your App</h1>
-      <button onClick={openPopup}>New Entry</button>
-
-      {isPopupOpen && (
+      <h1>Inventory Manager</h1>
+      <form>
+        <ol>
+          {ItemArray.map(item => {
+            return <div>
+              <li>{item.ItemName}</li>
+            </div>
+          })}
+        </ol>
+      </form>
+      <button onClick={openWindow}>New Entry</button>
+      {AddWindow ?  (
         <div className="popup-overlay">
           <div className="popup">
-            <span className="close-btn" onClick={closePopup}>
-              &times;
-            </span>
-            <AddItem closePopup={closePopup} />
+            <button className="close-btn" onClick={closeWindow}>Return</button>
+            <AddItem closeWindow={closeWindow} />
           </div>
         </div>
-      )}
+      ): null }
     </div>
+    </>
   );
 };
 
