3fd30545db7f42a26307eee0cfb8b393c7ee0e6b
GogogoDavis <burkedavis@yahoo.com>
2023-12-06 20:49:11 -0700

    final, hope it works

--- backend/seeds/01_initial_users.js
+++ backend/seeds/01_initial_users.js
@@ -8,7 +8,9 @@ exports.seed = async function(knex) {
   await knex('users_table').insert([
     {id: 1, FirstName: 'Borp', LastName: 'Bavis', username: 'DaBavester', password: 'IBave'},
     {id: 2, FirstName: 'Borp', LastName: 'Tavis', username: 'DaTavester', password: 'ITave'},
-    {id: 3, FirstName: 'Borp', LastName: 'Ravis', username: 'DaRavester', password: 'IParty'}
+    {id: 3, FirstName: 'Borp', LastName: 'Ravis', username: 'DaRavester', password: 'IParty'},
+    {id: 4, FirstName: 'Icecream', LastName: 'Hater', username: 'probablyWegenke', password: 'IHateIcecream'},
+    {id: 5, FirstName: 'Icecream', LastName: 'Lover', username: 'probablyBurke', password: 'ILoveIcecream'},
 
   ]);
 };
--- backend/seeds/02_initial_items.js
+++ backend/seeds/02_initial_items.js
@@ -6,10 +6,11 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('items_table').del()
   await knex('items_table').insert([
-    {id: 1, userId: 3, ItemName: 'pickle', description: 'why it spicy', quantity: 2},
+    {id: 1, userId: 3, ItemName: 'salt', description: 'some people find me spicy', quantity: 2},
     {id: 2, userId: 2, ItemName: 'onion', description: 'i cry', quantity: 2},
-    {id: 3, userId: 1, ItemName: 'carrot', description: 'my eyes!', quantity: 2},
-    {id: 4, userId: 2, ItemName: 'cat food', description: 'how they eat this?', quantity: 2},
+    {id: 3, userId: 1, ItemName: 'b-52', description: 'what is my goal with this inventory?', quantity: 2},
+    {id: 4, userId: 4, ItemName: 'Quaker Oats', description: 'Now with 100% less Quaker', quantity: 2},
+    {id: 5, userId: 5, ItemName: 'Present', description: 'This is only for Wegenke.....(stop reading if you\'re not Wegenke) Its IceCream....yay!' , quantity: 2},
     
   ]);
 };
